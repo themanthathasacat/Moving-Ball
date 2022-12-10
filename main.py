ff3d7d322e28f2e6bf975033845023962afe55d2
CodingCat <116771936+themanthathasacat@users.noreply.github.com>
2022-12-09 19:24:51 -0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 import turtle
 from random import random
 
-
-
-
 def show_circle(t, color, pencolor, radius):
   t.pencolor(pencolor)
   t.fillcolor(color)
@@ -24,626 +21,85 @@ if __name__ == '__main__':
 def down():
   t.setheading(270)
 
-def save():
-    t.setheading(270)
+
+
 
 def left():
   t.setheading(180)
 
-def wave():
-    t.setheading(90)
+
+
+
+
 
 def right():
   t.setheading(0)
 
-def an():
-    t.setheading(0)
+
 
 s = turtle.Screen()
 s.bgcolor("Black")
 s.tracer(0)
 SCREEN_HEIGHT = 600
-SCREEN_WIDTH = 800
-step = 0.1
+SCREEN_WIDTH = 600
+RADIUS = 25
+
 s.setup(SCREEN_HEIGHT, SCREEN_WIDTH)
 s.listen()
 s.onkey(up, 'Up')
 s.onkey(down, 'Down')
 s.onkey(left, 'Left')
 s.onkey(right, 'Right')
-s.onkey(wave,'w')
-s.onkey(save, 's')
-s.onkey(an, 'a')
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+s.onkey(up,'w')
+s.onkey(down, 's')
+s.onkey(left, 'a')
+s.onkey(right, 'd')
 x = 0
+y = 0
+step = 0.1
+t.setheading(90)
+
+
 colors = ["white", "red", "orange", "yellow", "green", "blue", "purple"]
 ballcolor = "white"
 while True:
     t.clear()
-    show_circle(t, ballcolor, ballcolor, 25)
+    show_circle(t, ballcolor, ballcolor, RADIUS)
     s.update()
     t.forward(step)
-    x = x + step
+    heading = t.heading()
+    if heading == 0:
+        x = x + step
+    elif heading == 180:
+        x = x - step
+    elif heading == 90:
+        y = y + step
+    elif heading == 270:
+        y = y - step
+
+    print(x, y, heading)
+
     if x >= SCREEN_WIDTH/2:
       x = -SCREEN_WIDTH/2
       ballcolor = "red"
-      t.goto(-SCREEN_WIDTH/2, 0)
+      t.goto(x, y)
       t.forward(step)
 
+    elif x  <= -SCREEN_WIDTH/2:
+        x = SCREEN_WIDTH /2
+        ballcolor = "orange"
+        t.goto(x, y)
+        t.forward(-step)
 
+    if y >= SCREEN_HEIGHT/2:
+        y = -SCREEN_HEIGHT / 2
+        ballcolor = "yellow"
+        t.goto(x, y)
+        t.forward(step)
 
-
-
+    elif y <= -SCREEN_HEIGHT/2:
+        y = SCREEN_HEIGHT / 2
+        ballcolor = "green"
+        t.goto(x, y)
+        t.forward(-step)
 
